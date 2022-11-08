a8a7f9cada95b3234f07e0b1037587a3eadaddb1
Eli Henderson <eli.i.henderson95@gmail.com>
2022-11-08 06:09:17 +0000

    Update 6 files

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import User from "../utils/user.png";
+import { ImLinkedin, ImYoutube, ImFacebook } from "react-icons/im";
+import Image from "next/image";
 
 const LeftSidebar = () => {
-  return (
-    <div>LeftSidebar</div>
-  )
-}
+  const menuItmes = [
+    "Home",
+    "Contact Us",
+    "Manage Patients",
+    "Settings",
+    <button
+      className="
+      border-2
+      p-3
+      w-[150px]
+      text-2xl
+      rounded-md
+      border-[#e85538]
+      text-[#e85538]
+      hover:text-[#ffffff]
+      hover:bg-[#f29b8a]
+      cursor-pointer
+      "
+    >
+      Logout
+    </button>,
+  ];
 
-export default LeftSidebar
+  return (
+    <div
+      className="
+        border-r-2
+        h-full
+        w-[20%]
+        items-center
+        flex
+        flex-col
+        hidden
+        lg:block
+      "
+    >
+      <div className="border-b-2 border-gray-200 h-[20%] flex-col flex items-center justify-center">
+        <Image src={User} alt="profile Image" className="w-[64px] " />
+
+        <h2 className="text-2xl font-medium font-sans">Hello, John</h2>
+      </div>
+
+      <div className="border-b-2 h-[65%] flex flex-col items-center justify-evenly text-xl border-gray-200">
+        {menuItmes &&
+          menuItmes.map((item, index) => (
+            <div
+              key={index}
+              className="hover:text-orange-500 w-full text-center  cursor-pointer"
+            >
+              <h2 className="text-2xl font-medium font-sans">{item}</h2>
+            </div>
+          ))}
+      </div>
+      <div className="h-[15%] flex items-center justify-evenly">
+        <ImFacebook className="text-[2em] text-blue-600 cursor-pointer" />
+        <ImLinkedin className="text-[2em] text-blue-900 cursor-pointer" />
+        <ImYoutube className="text-[2em] text-red-600   cursor-pointer" />
+      </div>
+    </div>
+  );
+};
+
+export default LeftSidebar;
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { JSXElementConstructor } from "react";
+import { JsxElement } from "typescript";
+
+type NotificationType = {
+  text: string;
+  color: string;
+};
 
 const RightSidebar = () => {
+  const notifs = [
+    {
+      text: "Form Error",
+
+      color: "red",
+    },
+    {
+      text: "New Patient Added",
+
+      color: "green",
+    },
+    {
+      text: "Date reminder",
+
+      color: "yellow",
+    },
+  ];
+
   return (
     <div
       className="
         border-l-2
         h-full
         w-[20%]
-        bg-white
         items-center
         flex
         flex-col
@@ -15,13 +38,71 @@ const RightSidebar = () => {
         lg:block
         "
     >
-      <div>
+      <div
+        className="
+      items-center 
+      flex 
+      flex-col 
+      h-[30%]
+      border-b-2
+      border-gray-200
+      "
+      >
+        <p className="text-2xl">Notifications</p>
+
+        <div className="w-full">
+          {notifs.map((item, index) => (
+            <NotificationItem text={item.text} key={index} color={item.color} />
+          ))}
+        </div>
+      </div>
+      <div
+        className="
+      items-center 
+      flex 
+      flex-col
+      h-[30%]
+      border-b-2
+      border-gray-200
+      
+      "
+      >
+        <p className="text-2xl">F.A.Q</p>
+      </div>
+      <div className="h-[40%]">
         <iframe
           src="https://freedomcare.com/blog/"
           title="freedom care blog"
-          className="w-full h-[400px]"
+          className="w-full h-full"
         ></iframe>
       </div>
+    </div>
+  );
+};
+
+const NotificationItem = ({ text, color }: NotificationType) => {
+  return (
+    <div
+      className={`
+  border-2 
+  border-${color}-500 
+  p-1
+  w-[80%]
+  mt-2
+  text-${color}-500
+  rounded-md
+  flex
+  flex-col
+  items-center
+  justify-center
+  hover:text-${color}-700
+  hover:bg-${color}-200
+  font-semibold
+  cursor-pointer
+  mx-auto
+  `}
+    >
+      {text}
     </div>
   );
 };
