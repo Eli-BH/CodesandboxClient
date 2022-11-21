9650765af103f160a0bd4f979702a1b0a032935d
ciararodriguez20 <crodriguez@freedomcareny.com>
2022-11-21 09:53:41 -0500

    Settings page created

--- pages/SettingsPage.tsx
+++ pages/SettingsPage.tsx
@@ -9,12 +9,26 @@ import {
   MdOutlineArrowRight,
 } from "react-icons/md";
 import { AiOutlineDoubleRight } from "react-icons/ai";
+import React, {useState} from "react";
+
+import DemoSettings from '../components/settingsComponents/DemoSettings'
 
 const SettingsPage = (): JSX.Element => {
+  
+  const [active, setActive] = useState(false);
+  const [activeSetting, setActiveSetting] = useState("");
+
+  const settings:string[] = ["Change Name", "Change Address", "Change Number","Change Password", "Change Profile Photo"];
+  const links:string[] = ["name", "address", "number", "password", "photo"];
 
   const resolutions = {
     homeTab100 : "h-[95%]",
     homeTab150 : "h-[90%]"
+  }
+
+  const handleClick = (link) =>{
+    setActive(true);
+    setActiveSetting(link);
   }
 
   return (
@@ -58,36 +72,57 @@ const SettingsPage = (): JSX.Element => {
         >
           <div
             className="
+            bg-blue-50
+            rounded-b-lg
             h-full
             p-2
             lg:p-5
             flex
-            flex-col
             justify-items-start
             justify-evenly
+            relative
           "
           >
-            <div>
-              <h2>My Account</h2>
-              {/*Unorder List for Settings */}
-              <ul>
-                <li><a href="">Testing 1</a></li>
-                <li><a href="">Testing 2</a></li>
-                <li><a href="">Testing 3</a></li>
-                <li><a href="">Testing 4</a></li>
-              </ul>
+            {/*Unorder List for Settings */}
+          <ul className={`
+            ease-in
+            duration-300
+            h-full
+            w-1/2
+            flex
+            flex-col
+            justify-evenly
+            ${active ? "-translate-x-44 text-left z-0" : "translate-x-15 text-center z-10"}
+            transition-all
+            `}
+          >
+           {settings.map((item,index) =>(
+            <li key={index} className="hover:bg-gray-50 w-full cursor-pointer py-5 pl-2 rounded-l-sm ease-in duration-300" 
+            onClick={
+             ()=>handleClick(links[index])
+             //()=>console.log(links[index])
+            }>{item}</li>
+           ))}
+          </ul>
+            
+            <div className={`
+              ease-in
+              p-5
+              duration-300
+              bg-gray-50
+              h-[90%]
+              w-1/2
+              rounded-sm
+              absolute
+              ${active ? "translate-x-44 opacity-100 z-10" : "opacity-0 translate-x-0 z-0"}
+              transition-all
+              `}>
+                <DemoSettings setting={activeSetting}/>
             </div>
-            
-            
-            
-            <div>
-              <h2>Dark Mode</h2>
-              {/*Dark Mode Toggle will go here */}
-            </div>
 
 
 
-
+         
           </div>
         </Tab.Panel>
 
