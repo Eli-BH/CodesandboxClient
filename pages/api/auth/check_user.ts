af7d0f197f057d894fe8969c889ec3a382c17dfc
eli-bh <eli.i.henderson95@gmail.com>
2022-12-09 10:32:23 -0500

    TEST: check user

---
+++ pages/api/auth/check_user.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import pool from "../../../utils/connectSalesforce";
+import Caregiver from "../../../models/CaregiverModel";
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { email } = req.body;
+
+    if (!email) {
+        return res
+            .status(400)
+            .json({ success: false, message: "Invalid email or password", redirect: false });
+    }
+
+    try {
+
+        let foundUser = await pool.query('SELECT FirstName, LastName, Birthdate,Sfid, Email, caller_type_c, Primary_Language__c, Phone, RecordTypeId, MailingCity, MailingPostalCode, MailingState, MailingStreet FROM salesforce.Contact WHERE Email = $1', [email])
+
+        if (!foundUser) return res.status(404).json({ success: false, message: "No account", redirect: true })
+
+        foundUser = foundUser.rows[0]
+
+
+        res.status(200).json({ success: true, foundUser })
+
+
+    } catch (error: any) {
+        console.log(error)
+        res.status(500).json({ success: false, error: error.message })
+    }
+}
