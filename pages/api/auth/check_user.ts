dbc608949aa9486ec741e82c0bc9036e0f9d373e
eli-bh <eli.i.henderson95@gmail.com>
2022-12-09 10:36:59 -0500

    TEST: check user

--- pages/api/auth/check_user.ts
+++ pages/api/auth/check_user.ts
@@ -14,14 +14,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     try {
 
-        let foundUser = await pool.query('SELECT FirstName, LastName, Birthdate,Sfid, Email, caller_type_c, Primary_Language__c, Phone, RecordTypeId, MailingCity, MailingPostalCode, MailingState, MailingStreet FROM salesforce.Contact WHERE Email = $1', [email])
+        let foundUser = await pool.query('SELECT FirstName, LastName, Birthdate,Sfid, Email, Primary_Language__c, Phone, RecordTypeId, MailingCity, MailingPostalCode, MailingState, MailingStreet FROM salesforce.Contact WHERE Email = $1', [email])
+
+        let usageCheck = await pool.query("SELECT * FROM salesforce.Contact WHERE Email = $1", [email])
 
         if (!foundUser) return res.status(404).json({ success: false, message: "No account", redirect: true })
 
         foundUser = foundUser.rows[0]
 
 
-        res.status(200).json({ success: true, foundUser })
+        res.status(200).json({ success: true, foundUser, usageCheck })
 
 
     } catch (error: any) {
