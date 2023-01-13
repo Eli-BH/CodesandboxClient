db7eafceed225f4086a3c7f93824a25cc0a890ab
eli-bh <eli.i.henderson95@gmail.com>
2023-01-13 12:36:46 -0500

    Update: coming soon

--- pages/ManagePatientsPage.tsx
+++ pages/ManagePatientsPage.tsx
@@ -1,10 +1,14 @@
 const ManagePatientsPage = () => {
   return (
     <div className="bg-orange-50 h-full w-full rounded-md border-orange-200 border-4 flex flex-col items-center justify-center">
-      <p className="text-[32px]">Add a patient </p>
+      {/* <p className="text-[32px]">Add a patient </p>
       <p>
         To add a patient, all you need is their email address and phone number.
         We will alert your patient that you are ready to connect
+      </p> */}
+      <p className="text-[1.2rem] md:text-[3rem] lg:text-[4rem] font-bold text-orange-200 ">
+        {" "}
+        Coming soon
       </p>
     </div>
   );
