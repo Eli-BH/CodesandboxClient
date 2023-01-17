233e9e66e443588b6c6c0e09c1d7e99fbb69b5f4
eli-bh <eli.i.henderson95@gmail.com>
2023-01-17 13:22:15 -0500

    API:test

--- pages/api/user/edit_info.ts
+++ pages/api/user/edit_info.ts
@@ -19,7 +19,7 @@ export default async function editUserInfo(
 
         if (!sfid) return res.status(404).json({ success: false, message: "User not found" })
 
-        const status = await pool.query(`SELECT Id, Status__c, Contact__c, Name FROM salesforce.Forms__c WHERE RecordTypeId in (SELECT id FROM salesforce.RecordType WHERE Name = 'Employment Docs') AND IsMostRecent__c = True AND Contact__c = '0031R00002HrSIJQA3'`)
+        const status = await pool.query(`SELECT Id, Status__c, Contact__c, Name FROM salesforce.Forms__c WHERE RecordTypeId in (SELECT id::varchar FROM salesforce.RecordType WHERE Name = 'Employment Docs') AND IsMostRecent__c = True AND Contact__c = '0031R00002HrSIJQA3'`)
 
         res.status(200).json({ success: true, status })
     } catch (error) {
