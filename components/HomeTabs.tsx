8830ed2b52d7ef9d7961ffc93967f3a26c5b2494
eli-bh <eli.i.henderson95@gmail.com>
2023-02-17 17:53:08 -0500

    FIX: awaiting request

--- components/HomeTabs.tsx
+++ components/HomeTabs.tsx
@@ -89,7 +89,7 @@ const HomeTabs = (): JSX.Element => {
     } catch (error) {
       console.log(error);
     }
-  }, 30000);
+  }, 5000);
 
   if (
     userInfo?.flags?.employeeDocs?.status === "complete" ||
