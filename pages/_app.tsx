f11ba3d45cf62797a5b944162fd6f4731560f959
eli-bh <eli.i.henderson95@gmail.com>
2023-02-23 16:02:30 -0500

    UPDATE: Added mouseflow

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,6 +23,25 @@ export default function App({
 
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        {/* Add the Mouseflow tracking code here */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              <!-- Mouseflow tracking code -->
+              <script type="text/javascript">
+              window._mfq = window._mfq || [];
+              (function() {
+                var mf = document.createElement("script");
+                mf.type = "text/javascript"; mf.defer = true;
+                mf.src = "//cdn.mouseflow.com/projects/f06ba7d8-a7b6-48fe-941b-fdd2f0f7a01c.js";
+                document.getElementsByTagName("head")[0].appendChild(mf);
+              })();
+              </script>
+          `,
+          }}
+        />
+      </Head>
       <div
         className="
       w-[100vw]
