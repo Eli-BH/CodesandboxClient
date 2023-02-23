6f292108cc911844e0573ebdc8492bbe047dbeb3
eli-bh <eli.i.henderson95@gmail.com>
2023-02-23 16:35:32 -0500

    UPDATE: Added mouseflow

---
+++ pages/_document.tsx
@@ -0,0 +1,46 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                <!-- Mouseflow: begin -->
+                <script type="text/javascript">
+                  window._mfq = window._mfq || [];
+                  (function() {
+                    var mf = document.createElement("script");
+                    mf.type = "text/javascript"; mf.defer = true;
+                    mf.src = "//cdn.mouseflow.com/projects/f06ba7d8-a7b6-48fe-941b-fdd2f0f7a01c.js";
+                    document.getElementsByTagName("head")[0].appendChild(mf);
+                  })();
+                </script>
+                <!-- Mouseflow: end -->
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
