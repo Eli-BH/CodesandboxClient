f5179c39bec087438177970965c0b41911d77d27
eli-bh <eli.i.henderson95@gmail.com>
2023-03-07 14:00:59 -0500

    UI TESTING

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -69,7 +69,7 @@ const Navbar = () => {
 
   return (
     <div
-      className="border-b-2 w-full h-20 justify-center flex items-center border-gray-300 bg-white relative"
+      className="sm:border-none md:border-b-2  w-full h-20 justify-center flex items-center border-gray-300 bg-white relative"
       id="navbarContainer"
     >
       <CgMenu
@@ -121,7 +121,7 @@ const Navbar = () => {
         `}
         style={{ zIndex: "100" }}
       >
-        <div className="relative w-full h-full">
+        <div className="relative w-full h-screen bg-white">
           <CgClose
             className="
                     xs:top-2
